a0925028f857ccf570c57190bb9e96f61b1140cb
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-26 23:57:36 +0530

    sidebar progress

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,34 @@
 'use client'
 import { useGlobalState } from '@/app/context/globalProvider'
+import Image from 'next/image'
 import React from 'react'
 import styled from 'styled-components'
 
 const Sidebar = () => {
-  const {theme} = useGlobalState();
-  return <SidebarStyles theme={theme}>Sidebar</SidebarStyles>
+  const { theme } = useGlobalState();
+  return <SidebarStyles theme={theme}>
+    <div className="profile">
+      <div className="profile-overlay">
+        <div className="image">
+          <Image alt='profile' src={'/unnamed.jpg'} width={70} height={70} />
+        </div>
+        <h1>
+          <span>Tony</span>
+          <span>Stark</span>
+        </h1>
+      </div>
+      <ul className="nav-items">
+        
+      </ul>
+    </div>
+  </SidebarStyles>
 }
 
 const SidebarStyles = styled.nav`
   position: relative;   
-  width: ${(props)=>props.theme.borderWidth};
-  background-color: ${(props)=>props.theme.colorBg2};
-  border: 2px solid ${(props)=>props.theme.borderColor2};
+  width: ${(props) => props.theme.borderWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
   border-radius: 1rem;
 `;
 export default Sidebar
