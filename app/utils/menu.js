6a2ca4f18a918681e15a581f6588e85eb9c04cc6
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-27 23:42:33 +0530

    updated icons

--- app/utils/menu.js
+++ app/utils/menu.js
@@ -1,37 +1,32 @@
-import { FaHouse } from "react-icons/fa6";
+import { FaHouse } from "react-icons/fa";
 import { MdPriorityHigh } from "react-icons/md";
 import { FaCheckDouble } from "react-icons/fa";
 import { LuListTodo } from "react-icons/lu";
 
-const menu =[
+export const menu = [
     {
         id: 1,
-        title:"All Tasks",
+        title: "All Tasks",
         link: "/",
-        icon: FaHouse,
-        
+        icon: <FaHouse />,
     },
     {
         id: 2,
-        title:"Important",
+        title: "Important",
         link: "important",
-        icon: MdPriorityHigh,
-        
+        icon: <MdPriorityHigh />,
     },
     {
         id: 3,
-        title:"Completed",
+        title: "Completed",
         link: "/completed",
-        icon: FaCheckDouble,
-        
+        icon: <FaCheckDouble />,
     },
     {
         id: 4,
-        title:"Do it Now",
+        title: "Do it Now",
         link: "/todo",
-        icon: LuListTodo,
-        
+        icon: <LuListTodo />,
     },
-]
+];
 
-export default menu;
