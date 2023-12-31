a99034293ee65c37c09166f8f2500ca8babf3602
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-31 21:39:36 +0530

    sign up added

---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div><SignUp /></div>
+    )
+}
+
+export default page
