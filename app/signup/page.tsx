2da95cfeeef97221ebb7e83a18d97513b05483f6
Ramanand Thakur <ramanandt35@gmail.com>
2024-01-08 21:14:12 +0530

    signin, signup page styled

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div><SignUp /></div>
+        <div className='flex items-center justify-center h-full'><SignUp /></div>
     )
 }
 
